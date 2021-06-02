6b4e280b66f80ea05bba4d1287f7e8acec955fd5
I-Tang <itang.hiu@gmail.com>
2021-06-02 20:09:08 +0200

    Added selection of a sentence when a column is clicked

--- js/main.js
+++ js/main.js
@@ -43,12 +43,11 @@ $(function () { // a short-hand for: $(document).ready(function() { ... });
          }
          str = str + "</tr>"
          $("#horizontal_view").append(str);
-
      }
 
-
     // vertical display
-    function vertical_view() {
+    function vertical_view(sentence_index) {
+        $("#vertical_view").empty();
         for (i = 0; i < data.SENTENCES.length; i++) {
             sentence = data.SENTENCES[i]
             emotion = data.EMOTIONS[i]
@@ -57,15 +56,22 @@ $(function () { // a short-hand for: $(document).ready(function() { ... });
             A =  data.A[i]
             D =  data.D[i]
             var emotion_image_tag = '<img src="' + constants.IMAGE_DIR + '/' + emotion_string + '.png" width="15" height="15">'
-             color_v = constants.COLOR_VAD[V]
+            color_v = constants.COLOR_VAD[V]
             color_a = constants.COLOR_VAD[A]
             color_d = constants.COLOR_VAD[D]
+            color_v = Utils.updateColor(color_v.substring(1))
+            color_a = Utils.updateColor(color_a.substring(1))
+            color_d = Utils.updateColor(color_d.substring(1))
+
             var v_tag = '<td style="background-color:' + color_v + ';color:'+ color_v + ';">11</td>'
             var a_tag = '<td style="background-color:' + color_a + ';color:'+ color_a + ';">11</td>'
             var d_tag = '<td style="background-color:' + color_d + ';color:'+ color_d + ';">11</td>'
 
-            str = "<tr>"
-            + '<td style="background-color:#FFFFFF;">' + emotion_image_tag + "</td>"
+            str = '<tr>'
+            if ( (sentence_index !== null) && (sentence_index === i)) {
+                str = '<tr style="background-color:#ffc107;">'
+            }
+            str += '<td style="background-color:#FFFFFF;">' + emotion_image_tag + "</td>"
             + v_tag
             + a_tag
             + d_tag
@@ -77,5 +83,21 @@ $(function () { // a short-hand for: $(document).ready(function() { ... });
 
     horizontal_view();
     vertical_view();
+
+    //$( "#horizontal_view" ).click(function() {
+    //    alert( "Handler for .click() called." );
+
+    $("#horizontal_view tr:has(td)").mouseover(function(e) {
+         $(this).css("cursor", "pointer");
+    });
+
+    $("#horizontal_view tr:has(td)").click(function(e) {
+         $("#horizontal_view td").removeClass("highlight");
+         var clickedCell= $(e.target).closest("td");
+         //clickedCell.addClass("highlight");
+         var cell_index = e.target.cellIndex
+         vertical_view(cell_index);
+         //$("#spnText").html('Clicked table cell value is: <b> ' + clickedCell.text() + '</b>');
+    });
 
 });
